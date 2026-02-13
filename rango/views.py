4f50fe09395d3e98fe50eca0a4661c5291cc161e
Lewis Kirkbright <2996229k@student,gla.ac.uk>
2026-02-13 05:44:46 +0000

    Chapter 10: server-side sessions visit counter

--- rango/views.py
+++ rango/views.py
@@ -10,90 +10,71 @@ from rango.forms import UserForm, UserProfileForm
 from django.contrib.auth.decorators import login_required
 from datetime import datetime
 
-def visitor_cookie_handler(request, response):
-    # Get the number of visits to the site.
-    # If the cookie exists, cast it to an int.
-    # If it doesn't exist, default to 1.
-    visits = int(request.COOKIES.get('visits', '1'))
 
-    last_visit_cookie = request.COOKIES.get('last_visit', str(datetime.now()))
-    last_visit_time = datetime.strptime(last_visit_cookie[:-7], '%Y-%m-%d %H:%M:%S')
+def get_server_side_cookie(request, cookie, default_val=None):
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val
+    return val
+
+def visitor_cookie_handler(request):
+    visits = int(get_server_side_cookie(request, 'visits', '1'))
+
+    last_visit_cookie = get_server_side_cookie(
+        request,
+        'last_visit',
+        str(datetime.now())
+    )
+
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7],
+                                        '%Y-%m-%d %H:%M:%S')
 
     # If it's been more than a day since the last visit...
     if (datetime.now() - last_visit_time).days > 0:
         visits = visits + 1
-        # Update the last visit cookie now that we have updated the count
-        response.set_cookie('last_visit', str(datetime.now()))
+        request.session['last_visit'] = str(datetime.now())
     else:
-        # Set the last visit cookie
-        response.set_cookie('last_visit', last_visit_cookie)
+        request.session['last_visit'] = last_visit_cookie
 
-    # Update/set the visits cookie
-    response.set_cookie('visits', visits)
-
-
+    request.session['visits'] = visits
 
 def index(request):
-    # Top 5 categories by likes
     category_list = Category.objects.order_by('-likes')[:5]
-
-    # Top 5 pages by views
     page_list = Page.objects.order_by('-views')[:5]
 
-    context_dict = {}
-    context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
-    context_dict['categories'] = category_list
-    context_dict['pages'] = page_list
+    # Update visits in the session first
+    visitor_cookie_handler(request)
 
-    # Add visits into the context so index.html can show it.
-    context_dict['visits'] = int(request.COOKIES.get('visits', '1'))
+    context_dict = {
+        'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!',
+        'categories': category_list,
+        'pages': page_list,
+        'visits': request.session['visits'],
+    }
 
-    # Obtain our Response object early so we can add cookie information.
-    response = render(request, 'rango/index.html', context=context_dict)
-
-    # Call the helper function to handle the cookies
-    visitor_cookie_handler(request, response)
-
-    # Return response back to the user, updating any cookies that need changed.
-    return response
-
+    return render(request, 'rango/index.html', context=context_dict)
 
 
 def about(request):
-
-    return render(request, 'rango/about.html')
+    visitor_cookie_handler(request)
+    context_dict = {'visits': request.session['visits']}
+    return render(request, 'rango/about.html', context=context_dict)
 
 def show_category(request, category_name_slug):
-    # Create a context dictionary which we can pass
-    # to the template rendering engine.
     context_dict = {}
 
     try:
-        # Can we find a category name slug with the given name?
-        # If we can't, the .get() method raises a DoesNotExist exception.
-        # The .get() method returns one model instance or raises an exception.
         category = Category.objects.get(slug=category_name_slug)
-
-        # Retrieve all of the associated pages.
-        # The filter() will return a list of page objects or an empty list.
         pages = Page.objects.filter(category=category)
 
-        # Adds our results list to the template context under name pages.
-        context_dict['pages'] = pages
-
-        # We also add the category object from
-        # the database to the context dictionary.
-        # We'll use this in the template to verify that the category exists.
         context_dict['category'] = category
+        context_dict['pages'] = pages
     except Category.DoesNotExist:
-        # We get here if we didn't find the specified category.
-        # Don't do anything
-        # the template will display the "no category" message for us.
         context_dict['category'] = None
         context_dict['pages'] = None
 
-    # Go render the response and return it to the client.
     return render(request, 'rango/category.html', context=context_dict)
+
 
 @login_required
 def add_category(request):
