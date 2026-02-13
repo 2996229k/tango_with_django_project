3a98537db8bc20d5c2b0c7d103ff67def59a5e9e
Lewis Kirkbright <2996229k@student,gla.ac.uk>
2026-02-13 02:24:38 +0000

    Fix typo in add_category definition

--- rango/views.py
+++ rango/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, redirect
 from rango.forms import CategoryForm
 
-from django.http import HttpResponse
 from rango.models import Category
 from rango.models import Page
 from rango.forms import PageForm
@@ -62,23 +61,17 @@ def show_category(request, category_name_slug):
 def add_category(request):
     form = CategoryForm()
 
-    # A HTTP POST?
     if request.method == 'POST':
         form = CategoryForm(request.POST)
 
-        # Have we been provided with a valid form?
         if form.is_valid():
-            # Save the new category to the database.
             form.save(commit=True)
-
-            # Redirect back to index.
-            return redirect('/rango/')
+            return redirect(reverse('rango:index'))
         else:
-            # The supplied form contained errors - print to terminal.
             print(form.errors)
 
-    # Will handle bad form, new form, or no form supplied cases.
     return render(request, 'rango/add_category.html', {'form': form})
+
 
 
 
@@ -89,7 +82,8 @@ def add_page(request, category_name_slug):
         category = None
 
     if category is None:
-        return redirect('/rango/')
+        return redirect(reverse('rango:index'))
+
 
     form = PageForm()
 
